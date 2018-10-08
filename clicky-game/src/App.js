622304af3410b17650e7fe11fe9c4a5189e6a901
Kevin Erman <kevinerman@gmail.com>
2018-10-08 17:50:19 -0500

    score updates on click, img ID pushed to array. need logic to check array for duplicates

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -4,23 +4,46 @@ import GameCard from "./components/GameCard"
 import gamecards from "./gamecards.json"
 import Wrapper from "./components/Wrapper"
 
+let guessedArray = [];
+
 class App extends Component {
 
+  score = {
+    currentScore:0,
+    highScore:0,
+  }
+
   state = {
-    gamecards
+    gamecards,
   };
 
-  guessCard = id => {
-    const gamecards = this.state.gamecards;
+  updateScore = () => {
+    this.setState({ score: this.score.currentScore = guessedArray.length });
+    this.setState({ score: this.score.highScore = guessedArray.length });
+  }
 
-    console.log(gamecards[id-1]);
+  guessCard = id => {
+    console.log(this);
+
+    let guessID = this.state.gamecards[id-1].id;
+    // console.log(clickCheck);
+
+    guessedArray.push(guessID);
+    this.updateScore();
+    
+    console.log(this.score.currentScore);
+    // console.log(guessedArray);
+
   }
 
   render() {
     return (
       <Wrapper>
+      <h1>Score: {this.score.currentScore} </h1>
+      <h1> High Score: {this.score.highScore}</h1>
         {this.state.gamecards.map(gamecard => (
         <GameCard
+          state= {this.beenClicked}
           guessCard={this.guessCard}
           id={gamecard.id}
           key={gamecard.id}
--- clicky-game/src/components/GameCard/GameCard.js
+++ clicky-game/src/components/GameCard/GameCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./GameCard.css";
 
 const GameCard = props => (
-    <div className="card">
+    <div className="card" state="notGuessed">
         <div onClick={() => props.guessCard(props.id)}>
             <div className="img-container">
                 <img alt={props.name} src={props.image} id={props.id}/>
