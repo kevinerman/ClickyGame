c82efd2a55b70fdd6cb05badc3680a5f693b459b
Kevin Erman <kevinerman@gmail.com>
2018-10-09 11:00:32 -0500

    high score working. game taking 2 clicks to work

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -5,12 +5,22 @@ import gamecards from "./gamecards.json"
 import Wrapper from "./components/Wrapper"
 
 let guessedArray = [];
+let gameOn = false;
 
 class App extends Component {
 
+  arrayCheck = id => {
+    if (guessedArray.includes(id)) {
+      this.setState({ score: this.score.currentScore = 0 });
+      alert("you lose");
+      guessedArray = [];
+      return
+    }
+  }
+
   score = {
-    currentScore:0,
-    highScore:0,
+    currentScore: 0,
+    highScore: 0,
   }
 
   state = {
@@ -18,39 +28,46 @@ class App extends Component {
   };
 
   updateScore = () => {
+    gameOn = true;
     this.setState({ score: this.score.currentScore = guessedArray.length });
-    this.setState({ score: this.score.highScore = guessedArray.length });
+    if (this.score.highScore < guessedArray.length) {
+    this.setState({ score: this.score.highScore = guessedArray.length })
+    };
   }
 
   guessCard = id => {
-    console.log(this);
 
-    let guessID = this.state.gamecards[id-1].id;
+    let guessID = this.state.gamecards[id - 1].id;
     // console.log(clickCheck);
-
+    
+    if (gameOn === true) {
+    (this.arrayCheck(guessID));
+    gameOn = false;
+    } else if (gameOn === false) {
     guessedArray.push(guessID);
     this.updateScore();
-    
-    console.log(this.score.currentScore);
-    // console.log(guessedArray);
+    }
 
+    console.log(guessedArray);
   }
 
   render() {
     return (
-      <Wrapper>
-      <h1>Score: {this.score.currentScore} </h1>
-      <h1> High Score: {this.score.highScore}</h1>
-        {this.state.gamecards.map(gamecard => (
-        <GameCard
-          state= {this.beenClicked}
-          guessCard={this.guessCard}
-          id={gamecard.id}
-          key={gamecard.id}
-          image={gamecard.image}
-        />
-    ))}
-    </Wrapper>
+      <div>
+        <h1>Score: {this.score.currentScore} </h1>
+        <h1> High Score: {this.score.highScore}</h1>
+        <Wrapper>
+          {this.state.gamecards.map(gamecard => (
+            <GameCard
+              state={this.beenClicked}
+              guessCard={this.guessCard}
+              id={gamecard.id}
+              key={gamecard.id}
+              image={gamecard.image}
+            />
+          ))}
+        </Wrapper>
+      </div>
     );
   }
 }
