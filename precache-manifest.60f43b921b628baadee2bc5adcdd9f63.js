d1467d406d188f6275b811d96b80a79eb5145305
Kevin Erman <kevinerman@gmail.com>
2018-10-30 18:06:02 -0500

    Updates

---
+++ precache-manifest.60f43b921b628baadee2bc5adcdd9f63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "2edaf247c01ad1c80909",
+    "url": "/Clicky-Game/static/js/main.2edaf247.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/Clicky-Game/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "2edaf247c01ad1c80909",
+    "url": "/Clicky-Game/static/css/main.665ccfe0.chunk.css"
+  },
+  {
+    "revision": "c2486b2d08e2f35a24b482863edc3f69",
+    "url": "/Clicky-Game/index.html"
+  }
+];
